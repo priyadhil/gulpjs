56faf5bc14d2d6c27eafcf71e828142db3d76f3c
Mohanapriya <priya05mca@gmail.com>
2019-04-06 16:09:33 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+
+/* Top Level Functions
+gulp.task - Define tasks
+gulp.src - point to files to use
+gulp.dest - points to folder to output
+gulp.watch- watch files and folder for changes
+ */
+
+// logs messages
+gulp.task("message", function() {
+  return console.log("gulp is running...");
+});
+
+gulp.task("default", () => {
+  return console.log("Default gulp is running...");
+});
+
+gulp.task("copysrc", () => {
+  gulp.src("src/*.html").pipe(gulp.dest("dist"));
+});
+
+gulp.task("imageMin", () => {
+  gulp
+    .src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"));
+});
+
+gulp.task("minify", () => {
+  gulp
+    .src("src/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("sass", () => {
+  gulp
+    .src("src/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/css"));
+});
---
+++ src/js/file1.js
@@ -0,0 +1,5 @@
+// file 1
+console.log("this is file 1");
+
+//file 2
+console.log("this is file 1");
